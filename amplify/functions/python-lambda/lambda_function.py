8a3b569ff2921e9f86bb2a27a13731154a62bc03
b4demeister <lennart.thielemann@gmx.de>
2025-12-16 12:57:08 +0100

    Add Layer and XX route

--- amplify/functions/python-lambda/lambda_function.py
+++ amplify/functions/python-lambda/lambda_function.py
@@ -1,26 +1,23 @@
 import json
-import secrets
-import string
+import testlib  # <--- Das importieren wir jetzt aus dem Layer!
 
 def lambda_handler(event, context):
-    print("Gen 2 Event:", event)
-    length = 12
-    try:
-        if 'body' in event and event['body']:
-            body = json.loads(event['body'])
-            length = int(body.get('length', 12))
-            length = max(8, min(length, 64))
-    except:
-        pass
-
-    alphabet = string.ascii_letters + string.digits + "!@#$%^&*"
-    password = ''.join(secrets.choice(alphabet) for i in range(length))
+    print("Event:", event)
+    
+    # Wir holen uns die Nachricht aus dem Layer
+    layer_msg = testlib.sag_hallo()
+    
+    # Wir prüfen, welcher Pfad aufgerufen wurde (z.B. /generate oder /xx)
+    path = event.get('rawPath', 'unbekannt')
 
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Origin': '*'
         },
-        'body': json.dumps({'password': password, 'length': length})
+        'body': json.dumps({
+            'message': layer_msg,  # Hier beweisen wir, dass der Layer geht
+            'path_called': path    # Hier sehen wir, ob /xx aufgerufen wurde
+        })
     }
